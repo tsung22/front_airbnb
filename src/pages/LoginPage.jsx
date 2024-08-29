87fa32f9d6bb04629df882d09dbcd0a9afeb5245
Support TI <supportti@domaine-saint-dominique.com>
2024-08-29 11:50:00 -0400

    Creation page login avec un redireciton vers page register

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
     return (
         <>
-            <div>
-                <h2>This is login page</h2>
+            <div className="mt-4 grow flex items-center justify-around">
+                <div className="mb-60">
+                    <h1 className="text-4xl text-center font-bold mb-4">Login</h1>
+                    <form action="" className="max-w-md mx-auto">
+                        <input type="email" name="" id="" placeholder="Your courriel adress" />
+                        <input type="password" name="" id="" placeholder="***********" />
+                        <button type="submit" className="login">Login</button>
+                    </form>
+
+                    <div className="text-center py-2">
+                        <span className="text-center text-gray-500"> Don't have an account yet ?</span>
+                        <Link to={'/register'} className="font-bold mx-2 underline text">Register</Link>
+                    </div>
+                </div>
             </div>
         </>
     );
