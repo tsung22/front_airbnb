21f19ba49e5d3754302d78a76958359743795857
Support TI <supportti@domaine-saint-dominique.com>
2024-08-29 11:10:30 -0400

    Creation page Header et layout. ajout de ce dernier dans le page app.

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+    return (
+        <>
+            <div>
+                <h2>This is index page</h2>
+            </div>
+        </>
+    );
+}
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+    return (
+        <>
+            <div>
+                <h2>This is login page</h2>
+            </div>
+        </>
+    );
+}
