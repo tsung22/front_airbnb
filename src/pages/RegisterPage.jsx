f56f9e65fa658e87ebd2ed8696b7ed1ffe5f7c78
Yves Nicolas Kow Ying <kowyingyvesnicolas@gmail.com>
2024-08-31 17:07:57 -0400

    Installation axion

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,24 +1,54 @@
 import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
-    return (
-        <>
-            <div className="mt-4 grow flex items-center justify-around">
-                <div className="mb-60">
-                    <h1 className="text-4xl text-center font-bold mb-4">Register</h1>
-                    <form action="" className="max-w-md mx-auto">
-                        <input type="text" name="" id="" placeholder="Full name" />
-                        <input type="email" name="" id="" placeholder="Your courriel adress" />
-                        <input type="password" name="" id="" placeholder="***********" />
-                        <button type="submit" className="login">Register</button>
-                    </form>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-                    <div className="text-center py-2">
-                        <span className="text-center text-gray-500"> Already a member ? </span>
-                        <Link to={'/login'} className="font-bold mx-2 underline text">Login now</Link>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  function registerUser() {
+
+  }
+
+  return (
+    <>
+      <div className="mt-4 grow flex items-center justify-around">
+        <div className="mb-60">
+          <h1 className="text-4xl text-center font-bold mb-4">Register</h1>
+          <form onSubmit={registerUser} className="max-w-md mx-auto">
+            <input
+              type="text"
+              placeholder="Full name"
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+            />
+            <input
+              type="email"
+              value={email}
+              onChange={(ev) => setEmail(ev.target.value)}
+              placeholder="Your courriel adress"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(ev) => setPassword(ev.target.value)}
+              placeholder="***********"
+            />
+            <button className="login">
+              Register
+            </button>
+          </form>
+
+          <div className="text-center py-2">
+            <span className="text-center text-gray-500">
+              {" "}
+              Already a member ?{" "}
+            </span>
+            <Link to={"/login"} className="font-bold mx-2 underline text">
+              Login now
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
